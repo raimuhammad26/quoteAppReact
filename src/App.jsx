a9f96d31b639ed21961099be6885bbef1aa4d21b
mbs <mbsc26@live.com>
2024-08-28 05:04:04 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div id="quote-box">
+        <div id="text"></div>
+        <div id="author"></div>
+        <button id="new-quote">New Quote</button>
+        <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank">Tweet</a>
+      </div>
+    </>
+  )
+}
+
+export default App
