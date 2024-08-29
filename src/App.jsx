46d1e5d3d22a7cf062a107d57fd83c520ca4b890
mbs <mbsc26@live.com>
2024-08-29 16:28:19 +0500

    JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 function App() {
   return (
     <>
-      <div id="quote-box">
-        <div id="text"></div>
-        <div id="author"></div>
-        <button id="new-quote">New Quote</button>
-        <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank">Tweet</a>
+      <div className="container">
+        <div id="quote-box">
+          <div id="text">Quote Text</div>
+          <div id="author">Quote Author</div>
+          <button id="new-quote">Generate New Quote</button>
+          <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank">Tweet</a>
+        </div>
       </div>
     </>
   )
